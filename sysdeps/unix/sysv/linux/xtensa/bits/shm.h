47067a25866f2f871f46f0e2f2b417f42c6cd9f9
Max Filippov <jcmvbkbc@gmail.com>
2018-04-03 14:43:13 -0700

    fix shm.h
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- sysdeps/unix/sysv/linux/xtensa/bits/shm.h
+++ sysdeps/unix/sysv/linux/xtensa/bits/shm.h
@@ -48,23 +48,12 @@ struct shmid_ds
   {
     struct ipc_perm shm_perm;		/* operation permission struct */
     size_t shm_segsz;			/* size of segment in bytes */
-#if defined(__XTENSA_EL__)
     __time_t shm_atime;			/* time of last shmat() */
     unsigned long int __unused1;
     __time_t shm_dtime;			/* time of last shmdt() */
     unsigned long int __unused2;
     __time_t shm_ctime;			/* time of last change by shmctl() */
     unsigned long int __unused3;
-#elif defined(__XTENSA_EB__)
-    unsigned long int __unused1;
-    __time_t shm_atime;			/* time of last shmat() */
-    unsigned long int __unused2;
-    __time_t shm_dtime;			/* time of last shmdt() */
-    unsigned long int __unused3;
-    __time_t shm_ctime;			/* time of last change by shmctl() */
-#else
-# error endian order not defined
-#endif
     __pid_t shm_cpid;			/* pid of creator */
     __pid_t shm_lpid;			/* pid of last shmop */
     shmatt_t shm_nattch;		/* number of current attaches */
